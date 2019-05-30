d9e4bddab72313eed302d629e2c3de72040d14d3
raeginez <rachelginez@yahoo.com>
2019-05-30 12:09:41 +1000

    solved challenge

--- 11_fizzbuzz.rb
+++ 11_fizzbuzz.rb
@@ -26,4 +26,14 @@
 # Buzz
 # ...etc
 
-# Your code here
+for number in (1..100)
+    if number % 3 == 0 and number % 5 == 0 
+    puts "fizzbuzz"
+    elsif number % 3 == 0
+        puts "fizz"
+    elsif number % 5 == 0
+        puts "buzz"
+    else
+        puts number
+    end
+end
